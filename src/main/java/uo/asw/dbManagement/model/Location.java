7540d2a28fb04a05f665221b0ecc3e8c72515ed0
Jesús García Minas <UO250999@Uniovi.es>
2018-02-15 18:51:02 +0100

    Clase Location
    Añadida clase location que incluye la longitud y la latitud en la que se encuentra el agente

---
+++ src/main/java/uo/asw/dbManagement/model/Location.java
@@ -0,0 +1,91 @@
+package uo.asw.dbManagement.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "location")
+public class Location {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	
+	@NotNull
+	private double latitud;
+	
+	@NotNull
+	private double longitud;
+	
+	@OneToOne
+	private Citizen citizen;
+	
+	Location() {
+	}
+	
+	public Location (double latitud, double longitud) {
+		super();
+		this.latitud = latitud;
+		this.longitud = longitud;
+	}
+
+	public double getLatitud() {
+		return latitud;
+	}
+
+	public void setLatitud(double latitud) {
+		this.latitud = latitud;
+	}
+
+	public double getLongitud() {
+		return longitud;
+	}
+
+	public void setLongitud(double longitud) {
+		this.longitud = longitud;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((citizen == null) ? 0 : citizen.hashCode());
+		long temp;
+		temp = Double.doubleToLongBits(latitud);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		temp = Double.doubleToLongBits(longitud);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Location other = (Location) obj;
+		if (citizen == null) {
+			if (other.citizen != null)
+				return false;
+		} else if (!citizen.equals(other.citizen))
+			return false;
+		if (Double.doubleToLongBits(latitud) != Double.doubleToLongBits(other.latitud))
+			return false;
+		if (Double.doubleToLongBits(longitud) != Double.doubleToLongBits(other.longitud))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Location [latitud=" + latitud + ", longitud=" + longitud + ", citizen=" + citizen + "]";
+	}
+}
