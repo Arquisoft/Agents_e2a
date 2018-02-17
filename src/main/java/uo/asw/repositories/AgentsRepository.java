2a803f75c6bdd25c02cabb898ea3ce1b8cf635e2
Jesús García Minas <UO250999@uniovi.es>
2018-02-17 16:16:00 +0100

    Clase no necesaria
    Volvemos a buscar en la base de datos con la antigua funcionalidad

--- src/main/java/uo/asw/repositories/AgentsRepository.java
+++
@@ -1,8 +0,0 @@
-package uo.asw.repositories;
-
-import org.springframework.data.repository.CrudRepository;
-
-import uo.asw.dbManagement.model.Agent;
-
-public interface AgentsRepository extends CrudRepository<Agent, Long> {
-}
