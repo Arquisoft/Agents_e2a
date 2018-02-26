9bf92b7631f25e9725087690e15ea26579078902
Jesús García Minas <UO250999@uniovi.es>
2018-02-26 14:48:49 +0100

    Pruebas Agente

--- src/test/java/uo/asw/participants/controller/AgentTest.java
+++ src/test/java/uo/asw/participants/controller/AgentTest.java
@@ -22,15 +22,28 @@ public class AgentTest {
 	@Test
 	public void newCitizenTest() throws ParseException {
     	citizen = new Agent("1234", "juanTorre", "Person", 1, "12345678J", "Juan Antonio", "Vázquez", "juan@gmail.com");
-		
+		Agent citizen2 = new Agent("5678", "juanTorre", "Person", 1, "87654321Z", "Pedro", "Pedro", "pedro@gmail.com");
 		assertTrue(citizen != null);
-		
+		assertTrue(citizen.equals(citizen2));
+		assertEquals(citizen.hashCode(), citizen2.hashCode());
+
 		assertEquals("1234", citizen.getContraseña());
 		assertEquals("juanTorre", citizen.getNombreUsuario());
 		assertEquals("12345678J", citizen.getDni());
 		assertEquals("Juan Antonio", citizen.getNombre());
 		assertEquals("Vázquez", citizen.getApellidos());
 		assertEquals("juan@gmail.com", citizen.getEmail());
+		
+		citizen.setNombreUsuario("juantorre2");
+		assertEquals("juantorre2", citizen.getNombreUsuario());
+		citizen.setNombre("Juan");
+		assertEquals("Juan", citizen.getNombre());
+		citizen.setApellidos("Velázquez");
+		assertEquals("Velázquez", citizen.getApellidos());
+		citizen.setKind("Entity");
+		assertEquals("Entity", citizen.getKind());
+		citizen.setKindCode((long) 2);
+		assertEquals(new Long(2), citizen.getKindCode());
 	}
 	
 	
