d2a8341deb365335dcfbaa0932a9053f8235c5c2
Pelayo-Torre <36408805+Pelayo-Torre@users.noreply.github.com>
2018-03-02 23:35:01 +0100

    Eliminada clase WebController, que estaba dando errores

--- src/test/java/uo/asw/participants/controller/AllTests.java
+++ src/test/java/uo/asw/participants/controller/AllTests.java
@@ -3,7 +3,7 @@ package uo.asw.participants.controller;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
-
+ 
 @RunWith(Suite.class)
 @SuiteClasses({
 	
